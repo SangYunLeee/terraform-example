a8577133770b7112e4fb67061b365788d0318500
sangyun lee <sororiri@gmail.com>
2025-03-18 00:02:42 +0900

    feat: AWS 리전 변경 및 초기 설정 파일 추가
    - AWS 리전을 us-east-1에서 ap-northeast-2(서울)로 변경
    - .gitignore 파일 추가 (Terraform 관련 설정)
    - .terraform.lock.hcl 파일 추가
    - keypair 이름을 'eks-terraform-key'에서 'keypair-for-terraform'으로 변경
    - private-key 디렉토리 추가

--- eks/c4-07-ec2bastion-provisioners.tf
+++ eks/c4-07-ec2bastion-provisioners.tf
@@ -4,21 +4,21 @@ resource "null_resource" "copy_ec2_keys" {
   # Connection Block for Provisioners to connect to EC2 Instance
   connection {
     type     = "ssh"
-    host     = aws_eip.bastion_eip.public_ip    
+    host     = aws_eip.bastion_eip.public_ip
     user     = "ec2-user"
     password = ""
-    private_key = file("private-key/eks-terraform-key.pem")
-  }  
+    private_key = file("private-key/keypair-for-terraform.pem")
+  }
 
 ## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
   provisioner "file" {
-    source      = "private-key/eks-terraform-key.pem"
-    destination = "/tmp/eks-terraform-key.pem"
+    source      = "private-key/keypair-for-terraform.pem"
+    destination = "/tmp/keypair-for-terraform.pem"
   }
 ## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
   provisioner "remote-exec" {
     inline = [
-      "sudo chmod 400 /tmp/eks-terraform-key.pem"
+      "sudo chmod 400 /tmp/keypair-for-terraform.pem"
     ]
   }
 ## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
